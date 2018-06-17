8836d2453dd0d7f432f6ab4b19aeca2cc48081da
David Brown <dave@tucanoo.com>
2018-06-17 21:41:59 +0200

    jest and enzyme react tests introduced

---
+++ __tests__/test-example-work.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork from '../js/example_work';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const MYWORK = [
+  {
+    title:"The Legal Assistant, SAAS Legal Case Management",
+    image: {
+        desc:"The Legal Assistant, SAAS Legal Case Management developed with Grails, Groovy Java and lot's of love.",
+        src:"images/saas-legal-software-tla.png",
+        comment:""
+    }
+  }
+];
+
+describe ("Example work component", () => {
+  let component = shallow(<ExampleWork work={MYWORK} />);
+
+  it("should be a section element", () => {
+      expect(component.type()).toEqual('section');
+  })
+});
