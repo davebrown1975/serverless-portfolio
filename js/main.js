45e3cf8c5bc1779395113ef153e6de56af46fad4
David Brown <dave@tucanoo.com>
2018-06-17 17:29:19 +0200

    updated index to use react based work example.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example_work.js';
 
-console.log("Loaded react dom");
+const MYWORK = [
+  {
+    title:"The Legal Assistant, SAAS Legal Case Management",
+    image: {
+        desc:"The Legal Assistant, SAAS Legal Case Management developed with Grails, Groovy Java and lot's of love.",
+        src:"images/saas-legal-software-tla.png",
+        comment:""
+    }
+  },
+  {
+    title:"NFC",
+    image: {
+        desc:"New field work.",
+        src:"images/saas-legal-software-tla.png",
+        comment:""
+    }
+  }
+];
+
+ReactDOM.render(<ExampleWork work={MYWORK}/>, document.getElementById('example_work'));
